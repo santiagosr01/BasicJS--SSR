7ab4c0ad21010a8defeee52df129bfb230599e8e
Santiago <sansamuel2208@gmail.com>
2023-11-15 19:50:15 +0100

    Actividad1.js hecha

---
+++ actividad1.js
@@ -0,0 +1,12 @@
+const pt = require("prompt-sync")({sigint: true})
+
+const nombre = pt("¿Cuál es tu nombre? =>");
+const edad = pt("¿Cuál es tu edad? =>");
+
+
+if (edad >= 18) {
+  console.log(nombre + ", ya puedes conducir. Rotonda sin fuente, rotonda de frente");
+} else {
+  console.log(nombre + ", aun no puedes conducir chavalin");
+};
+
